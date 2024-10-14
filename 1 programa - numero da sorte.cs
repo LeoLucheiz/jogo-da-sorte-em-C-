7659600df04163fce6e5e79d9c3034939ec802bf
LeoLucheiz <leonardo.lucheiz@outlook.com>
2024-10-14 16:38:56 -0300

    Add files via upload

---
+++ 1 programa - numero da sorte.cs
@@ -0,0 +1,30 @@
+﻿Random random = new Random();
+
+int numeroScreto = Convert.ToInt32(random.Next(1, 100).ToString()); 
+Console.WriteLine(numeroScreto);
+
+
+do {
+    Console.WriteLine("Digite o número secreto entre 1 a 100");
+    int numeroDigitado = int.Parse(Console.ReadLine()!);
+
+    if (numeroDigitado == numeroScreto) {
+        Console.WriteLine("Parabens, você achou o numero secreto que é " + numeroScreto + " Fim de jogo");
+        break;
+
+    } else if (numeroDigitado < numeroScreto) {
+        Console.WriteLine(" Errou, o numero secreto é maior que " + numeroDigitado);
+    } else {
+        Console.WriteLine(" Errou, o numero secreto é menor que " + numeroDigitado);
+    }
+} while (true);
+
+
+
+
+
+
+
+
+
+
